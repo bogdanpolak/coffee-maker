7ae07760288f11cf2034c078f6c2c4f7edad2123
Bogdan Polak <bogdan.polak@bsc.com.pl>
2020-02-28 22:29:18 +0000

    Added data driven tests BrewAndVerifyBehaviour

--- Test.CoffeeMachine.pas
+++ Test.CoffeeMachine.pas
@@ -29,8 +29,12 @@ type
     [TearDown]
     procedure TearDown;
     [Test]
-    [TestCase('VerifyDoppioCoffee', 'csDoppio')]
-    procedure BrewAndVerifyBehaviour(const aCoffeeSelection: TCoffeeSelection);
+    [TestCase('Ristretto', 'csRistretto,7,gsSuperFine,20')]
+    [TestCase('Espresso', 'csEspresso,8,gsFine,30')]
+    [TestCase('Doppio', 'csDoppio,16,gsFine,60')]
+    [TestCase('Lungo', 'csLungo,8,gsFine,45')]
+    procedure BrewAndVerifyBehaviour(const aCoffeeSelection: TCoffeeSelection;
+      aCoffeeWeight: double; aGrindSize: TGrindSize; aWaterAmount: double);
   published
     procedure NotReadyToBrewCoffee;
     procedure BrewingEspresso;
@@ -79,13 +83,34 @@ begin
 end;
 
 procedure TestCoffeeMaker.BrewAndVerifyBehaviour(const aCoffeeSelection
-  : TCoffeeSelection);
+  : TCoffeeSelection; aCoffeeWeight: double; aGrindSize: TGrindSize; aWaterAmount: double);
+var
+  aCoffee: TCoffee;
 begin
-  Assert.AreEqual(csDoppio, aCoffeeSelection);
+  fMachineTester.SetUp.WillReturn(True).When.IsReadyToBrewCoffee;
+
+  fMachineTester.SetUp.Expect.Once.When.IsReadyToBrewCoffee;
+  fGrinder.SetUp.Expect.Once.When.SetGrindSize(aGrindSize);
+  fGrinder.SetUp.Expect.Once.When.GrindCoffeeBeans(aCoffeeWeight {mg coffee});
+  fBrewingUnit.SetUp.Expect.Once.When.PressCoffe;
+  fBrewingUnit.SetUp.Expect.Once.When.BrewWater(aWaterAmount {ml water});
+  fBrewingUnit.SetUp.Expect.Once.When.PressWater(9.0 {bar});
+  fBrewingUnit.SetUp.Expect.Once.When.TrashCoffe;
+  fUserPanel.SetUp.Expect.Exactly(1).When.CoffeeInProgress(True);
+  fUserPanel.SetUp.Expect.Exactly(1).When.CoffeeInProgress(False);
+
+  aCoffee := fCoffeeMachine.BrewCoffee(aCoffeeSelection);
+
+  Assert.AreEqual(aCoffeeSelection, aCoffee.Selection);
+  Assert.AreEqual(aWaterAmount, aCoffee.WaterML);
+  fMachineTester.Verify;
+  fGrinder.Verify;
+  fBrewingUnit.Verify;
+  fUserPanel.Verify;
 end;
 
 initialization
 
-TDUnitX.RegisterTestFixture(TestCoffeeMaker, 'Test CoffeeMaker');
+TDUnitX.RegisterTestFixture(TestCoffeeMaker);
 
 end.
